2803651466647e9e3b6544ed4ddc042bd7bab9aa
Lucas Vieira <lucasvtiradentes@gmail.com>
2024-03-10 11:52:10 -0300

    feat: add dependent plugin configs

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -3,6 +3,9 @@ import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import NotesManager from '../main';
 
 export type TPluginSettings = {
+  use_file_sufix: boolean;
+  hide_file_sufix: boolean;
+  file_sufix: string;
   one_level_note_first_column_name: string;
   one_level_note_second_column_name: string;
   two_level_note_first_column_name: string;
@@ -11,6 +14,9 @@ export type TPluginSettings = {
 };
 
 export const DEFAULT_SETTINGS: TPluginSettings = {
+  use_file_sufix: true,
+  hide_file_sufix: true,
+  file_sufix: '.nm',
   one_level_note_first_column_name: 'THEME',
   one_level_note_second_column_name: 'LINK',
   two_level_note_first_column_name: 'THEME',
@@ -39,10 +45,81 @@ export class NotesManagerSettings<T extends PluginWithSettings> extends PluginSe
   display(): void {
     const { containerEl } = this;
     containerEl.empty();
-
     const settings = this.plugin.settings;
+
+    const elementClasses = {
+      fileSufix: 'fileSufix',
+      hideFileSufix: 'hideFileSufix'
+    } as const;
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    const modifyDependentSettings = (mode: 'show' | 'hide') => {
+      const hideFileSufixEl = containerEl.getElementsByClassName(elementClasses.hideFileSufix)[0];
+      const fileSufixEl = containerEl.getElementsByClassName(elementClasses.fileSufix)[0];
+
+      if (mode === 'show') {
+        hideFileSufixEl?.setAttribute('style', 'display: block;');
+        fileSufixEl?.setAttribute('style', 'display: block;');
+      } else if (mode === 'hide') {
+        hideFileSufixEl?.setAttribute('style', 'display: none;');
+        fileSufixEl?.setAttribute('style', 'display: none;');
+      }
+    };
+
     const settings_section01 = containerEl.createEl('div', { cls: 'settings_section' });
-    settings_section01.createEl('div', { text: 'One level notes configs', cls: 'settings_section_title' });
+    settings_section01.createEl('div', { text: 'General configs', cls: 'settings_section_title' });
+
+    new Setting(containerEl).setName('Use file sufix').addToggle((toggle) =>
+      toggle.setValue(settings.use_file_sufix).onChange(async (value) => {
+        settings.use_file_sufix = value;
+        await this.plugin.saveSettings();
+        if (value) {
+          modifyDependentSettings('show');
+        } else {
+          modifyDependentSettings('hide');
+        }
+      })
+    );
+
+    new Setting(containerEl)
+      .setName('Hide file sufix')
+      .setClass(elementClasses.hideFileSufix)
+      .addToggle((toggle) =>
+        toggle.setValue(settings.hide_file_sufix).onChange(async (value) => {
+          settings.hide_file_sufix = value;
+          await this.plugin.saveSettings();
+        })
+      );
+
+    new Setting(containerEl)
+      .setName('File sufix')
+      .setClass(elementClasses.fileSufix)
+      .addText((text) =>
+        text
+          .setPlaceholder('Type the file sufix')
+          .setValue(settings.file_sufix)
+          .onChange(async (value) => {
+            value = value.replace(/[^a-zA-Z]/g, '');
+
+            if (!value.startsWith('.')) {
+              value = '.' + value;
+            }
+            settings.file_sufix = value;
+            await this.plugin.saveSettings();
+          })
+      );
+
+    if (settings.use_file_sufix) {
+      modifyDependentSettings('show');
+    } else {
+      modifyDependentSettings('hide');
+    }
+
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    const settings_section02 = containerEl.createEl('div', { cls: 'settings_section' });
+    settings_section02.createEl('div', { text: 'One level notes configs', cls: 'settings_section_title' });
 
     new Setting(containerEl).setName('First column name').addText((text) =>
       text
@@ -64,8 +141,10 @@ export class NotesManagerSettings<T extends PluginWithSettings> extends PluginSe
         })
     );
 
-    const settings_section02 = containerEl.createEl('div', { cls: 'settings_section' });
-    settings_section02.createEl('div', { text: 'Two level notes configs', cls: 'settings_section_title' });
+    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    const settings_section03 = containerEl.createEl('div', { cls: 'settings_section' });
+    settings_section03.createEl('div', { text: 'Two level notes configs', cls: 'settings_section_title' });
 
     new Setting(containerEl).setName('First column name').addText((text) =>
       text
